1d28f0dab16bd11f0bce348b860cd7be40c4d503
jiyoon <rhjiyoon@yonsei.ac.kr>
2025-06-12 00:59:46 +0900

    update CORS configuration to use dynamic frontend URL from application.yml

--- backend/byeoltago-api/src/main/java/com/kakaoimpact/byeoltago_api/config/WebConfig.java
+++ backend/byeoltago-api/src/main/java/com/kakaoimpact/byeoltago_api/config/WebConfig.java
@@ -1,16 +1,22 @@
 package com.kakaoimpact.byeoltago_api.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${custom.cors.frontend-url}")
+    private String frontendUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://jiy0-0nv.github.io/project-byeoltago/frontend") // 배포된 프론트 주소
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
